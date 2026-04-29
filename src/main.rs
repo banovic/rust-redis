29678d1f589e088000b69fef10696f6b5d036300
Branislav Anovic <branislav.anovic@gmail.com>
2026-04-29 02:46:49 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -912,7 +912,8 @@ async fn process_xadd(
         // return Err(ParseError {
         //     message: "XADD: key already exists".to_string(),
         // });
-        return Ok(Resp::BulkString(b"Error 123".to_vec()));
+        //return Ok(Resp::BulkString(b"Error 123".to_vec()));
+        return Ok(Resp::Null);
     }
     if let Some((latest, _)) = store.streams.get(name).unwrap().last_key_value() {
         if &key < latest {
