69c6085e333db37758c66d19734b51d11befa65d
Branislav Anovic <branislav.anovic@gmail.com>
2026-06-27 01:04:36 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1780,9 +1780,9 @@ async fn handle_client(
                     if let Some(encoded_command) = command.encode_to_bytes() {
                         println!("Replica (master process, client connection handler) sending encoded command: {:?}", encoded_command);
                         let x1 = stream.write_all(&encoded_command).await;
-                        //println!("Result x1: {:?}", x1);
+                        println!("Result x1: {:?}", x1);
                         let x2 = stream.flush().await;
-                        //println!("Result x2: {:?}", x2);
+                        println!("Result x2: {:?}", x2);
                         if let Some(reply_back_to_master_tx) = reply_tx {
                             waiters_queue.push_back(reply_back_to_master_tx);
                             println!("Replica client will notify master (same process), queue: {:?}", waiters_queue);
