84af1411e1aedb5fad1ba7309a44c5c1a50dea87
Branislav Anovic <branislav.anovic@gmail.com>
2026-07-12 16:51:48 +0000

    codecrafters submit [skip ci]

--- src/aof.rs
+++ src/aof.rs
@@ -37,10 +37,9 @@ impl Aof {
             }
 
             // Create file
-            let basefilename = self.appendfilename.split(".").nth(0).unwrap();
-            let filename = format!("{}/{}.1.incr.aof", dirname, basefilename);
-            println!("[aof] self.appendfilename: {}", self.appendfilename);
-            println!("[aof] creating initial aof: {}", filename);
+            let filename = format!("{}{}.1.incr.aof", dirname, self.appendfilename);
+            // println!("[aof] self.appendfilename: {}", self.appendfilename);
+            // println!("[aof] creating initial aof: {}", filename);
             let mut file = File::create(filename).await.unwrap();
         }
     }
