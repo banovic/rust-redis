7022f0c25d5cac25c17a73324a3ad149f967ed24
Branislav Anovic <branislav.anovic@gmail.com>
2026-07-12 17:20:02 +0000

    codecrafters submit [skip ci]

--- src/aof.rs
+++ src/aof.rs
@@ -41,18 +41,13 @@ impl Aof {
 
             // Create file
             let filename = format!("{}{}.1.incr.aof", dirname, self.appendfilename);
-            let mut file = File::create(filename).await.unwrap();
+            let mut file = File::create(filename.clone()).await.unwrap();
 
             // Create manifest file
             let mf_base_filename = format!("{}.manifest", self.appendfilename);
             let mf_filename = format!("{}{}", dirname, mf_base_filename);
-            println!("[aof] creating manifest file: {}", mf_filename);
             let mut mf_file = File::create(mf_filename).await.unwrap();
-            let line = format!("file {} seq 1 type i", mf_base_filename);
-            println!(
-                "[aof] writing line in manifest file: {}, {}",
-                mf_base_filename, line
-            );
+            let line = format!("file {} seq 1 type i", filename);
             let _ = mf_file.write_all(line.as_bytes()).await.unwrap();
         }
     }
